8a0e66f14501249e4f0bf09228124e791fd5833a
lucasmarinskiba <116664832+lucasmarinskiba@users.noreply.github.com>
2025-01-20 19:02:51 -0300

    Create startingbot2.py

---
+++ startingchatbot.py
@@ -0,0 +1,11 @@
+question ="Hello! What is your name?"
+name = input("Hello! What is your name? ")
+print(f"Nice to meet you, {name}!")
+age_input = input("How old are you? ")
+age = int(age_input)
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+color = print("What's your favorite color?")
+color_input = input("")
+print(f"Oh, {color} is a beautiful color!")
